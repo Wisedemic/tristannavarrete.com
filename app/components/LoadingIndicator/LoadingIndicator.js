77720c4202bc30faf2c7e76b97a6d1d83f12f60d
Tristan Navarrete <navarrete.tristan@gmail.com>
2018-08-26 02:04:25 -0400

    Updated Loading Indicator

--- app/components/LoadingIndicator/LoadingIndicator.js
+++ app/components/LoadingIndicator/LoadingIndicator.js
@@ -2,16 +2,28 @@ import React from 'react';
 import './style.scss';
 
 const LoadingIndicator = () => (
-  <div className="loading-indicator">
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-  </div>
+	<section className="hero is-fullheight">
+		<div className="hero-body">
+			<div className="container has-text-centered">
+				<div className="columns">
+					<div className="column">
+						<h1 className="title">Loading ... </h1>
+						<h2 className="subtitle">This may take a moment</h2>
+					  <div className="loading-indicator">
+					    <div />
+					    <div />
+					    <div />
+					    <div />
+					    <div />
+					    <div />
+					    <div />
+					    <div />
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 );
 
 export default LoadingIndicator;
--- app/containers/Animation/Loadable.js
+++ app/containers/Animation/Loadable.js
@@ -1,13 +1,11 @@
-// /**
-//  * Asynchronously loads the component for HomePage
-//  */
-// import Loadable from 'react-loadable';
-//
-// import LoadingIndicator from 'components/LoadingIndicator';
-//
-// export default Loadable({
-//   loader: () => import('./index'),
-//   loading: LoadingIndicator,
-// });
+/**
+ * Asynchronously loads the component for HomePage
+ */
+import Loadable from 'react-loadable';
 
-export { default } from './index.js';
+import LoadingIndicator from 'components/LoadingIndicator';
+
+export default Loadable({
+  loader: () => import('./index'),
+  loading: LoadingIndicator,
+});
